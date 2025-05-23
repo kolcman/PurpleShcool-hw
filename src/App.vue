1a2fb22bad9bbea09eb3b7b5b333c89e81aecc8c
kolcman <m.kolcman@yandex.ru>
2025-05-23 13:27:12 +0300

    Complete
    ## Добавил:
    - view кнопки
    - стиль для кнопки
    - шрифты

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <template>
+    <main class="main">
+        <ButtonView>Начать игру</ButtonView>
+    </main>
 </template>
 
 
 <script setup>
-
+import ButtonView from './components/ButtonView.vue';
 
 </script>
 
 
-<style scoped></style>
+<style scoped>
+.main {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    width: 100vw;
+}
+</style>
--- src/components/ButtonView.vue
+++ src/components/ButtonView.vue
@@ -1,8 +1,26 @@
 <template>
-    <button></button>
+    <button class="btn">
+        <slot></slot>
+    </button>
 </template>
 
 <script setup>
+
 </script>
 
-<style scoped></style>
+<style scoped>
+.btn {
+    background-color: var(--color-btn);
+    width: 335px;
+    padding: 10px 16px;
+    border: none;
+    border-radius: 100px;
+    color: var(--color-secondery);
+    font-size: 24px;
+    line-height: 36px;
+}
+
+.btn:hover {
+    background-color: var(--color-btn-hover);
+}
+</style>
