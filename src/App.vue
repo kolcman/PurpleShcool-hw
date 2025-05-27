e7509fcc1122eb02f9c1512ce21737427083bb9a
kolcman <m.kolcman@yandex.ru>
2025-05-27 14:46:28 +0300

    Добавил
    - Вывод карточек через v-for
    - Условия для вывода карточек
    - Стили для  разных типов карточек.

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
     <Header />
     <main class="main">
         <!-- <Button aria-label="Начать новую игру">Начать игру</Button> -->
-        <Card v-bind="card" @flip-card="flipCard" />
+        <ul class="card-list">
+            <Card v-for="card in data" :key="card.num" v-bind="card" @flip-card="flipCard" />
+        </ul>
     </main>
 </template>
 
@@ -13,13 +15,39 @@ import Header from '@/components/Header.vue';
 import Card from '@/components/Card.vue'
 import { ref } from 'vue';
 
-const card = ref({
-    num: "01",
-    word: "word",
-    translation: "transl",
-    state: "open",
-    status: "pending"
-})
+
+const data = ref([
+    {
+        num: "01",
+        word: "Apple",
+        translation: "Яблоко",
+        state: false,
+        status: "pending"
+    },
+    {
+        num: "02",
+        word: "Green",
+        translation: "Зеленый",
+        state: true,
+        status: "pending"
+    },
+    {
+        num: "03",
+        word: "Army",
+        translation: "Армия",
+        state: true,
+        status: "right"
+    },
+    {
+        num: "04",
+        word: "Car",
+        translation: "Автомобиль",
+        state: true,
+        status: "wrong"
+    },
+
+])
+
 
 function flipCard() {
 
@@ -34,5 +62,11 @@ function flipCard() {
     justify-content: center;
     align-items: center;
     height: 100dvh;
+}
+
+.card-list {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 66px 107px;
 }
 </style>
